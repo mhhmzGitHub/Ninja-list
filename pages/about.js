42050f5745abae2b55d5cfb1cb4693bfa4bef9c8
mhhmzGitHub <mhhmz0@gmail.com>
2021-02-08 14:40:40 +0200

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+const About = () => {
+    return ( 
+        <>
+        <Head>
+          <title>Ninga List | About</title>
+         <meta name="keywords" content="ninjas" />
+    
+        </Head>
+        <div>
+            <h1>About Page</h1>
+            <p>loremfgdfgdfgdg </p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
